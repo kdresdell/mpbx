483688f048718dc3826b966de8d8c12ca9ee8909
Ken Dresdell <kdresdell@gmail.com>
2015-01-22 22:33:12 -0500

    Ajout des fichiers conf

--- SetupMPBX.sh
+++ SetupMPBX.sh
@@ -116,20 +116,13 @@ else
 fi
 
 
-apt-get install build-essential \
-                subversion \ 
-                libncurses-dev \
-                libssl-dev \
-                libxml2-dev \
-                sqlite3 \
-                libsqlite3-dev 
+apt-get install build-essential subversion libncurses-dev libssl-dev libxml2-dev sqlite3 libsqlite3-dev uuid uuid-dev libjansson-dev
 
-
-wget http://downloads.asterisk.org/pub/telephony/asterisk/asterisk-13-current.tar.gz
+#wget http://downloads.asterisk.org/pub/telephony/asterisk/asterisk-13-current.tar.gz
 
 
 tar zxvf asterisk-13-current.tar.gz
-cd asterisk-13-current
+cd asterisk-13.1.0
 
 
 contrib/scripts/install_prereq install
